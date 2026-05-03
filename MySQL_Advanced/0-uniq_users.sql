395c036cd897b0292f617743eae758b4c06efb40
Emil-610 <codesolve08112022@gmail.com>
2026-05-03 18:51:40 +0400

    Update 0-uniq_users.sql

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
--- MYSQL advanced project
--- script that creates a users table
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
+-- Creates a table 'users' with unique email constraint
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
